17b8c39cdb8eb1c06e79a6b1f268a4fab0f7d1e2
Katrina Peterson <peterkat009@edmonds15.org>
2019-07-31 09:48:26 -0700

    project init

---
+++ sss.js
@@ -0,0 +1,16 @@
+
+
+function two() {
+  document.getElementById("monitor").src = "https://myedmondsnews.com/wp-content/uploads/2018/11/181027-015.jpg";
+  document.getElementById("caption").innerHTML = "Students monitoring the stream and looking for salmon"
+}
+
+function one() {
+  document.getElementById("monitor").src = "https://edmondsbeacon.villagesoup.com/media/Common/BeaconPublishing/2018/4/11/2420908/t1200-Emily%20Salmon.png";
+  document.getElementById("caption").innerHTML = "SSS members collecting water quality data"
+}
+
+function three() {
+  document.getElementById("monitor").src = "https://myedmondsnews.com/wp-content/uploads/2019/01/190111-089.jpg";
+  document.getElementById("caption").innerHTML = "SSS members placing a hatchbox in Shell Creek"
+}
